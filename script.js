0682fd04dcbb444960d301962aa01fd0bed310d4
Milan fataniya <155900672+milanfataniya@users.noreply.github.com>
2026-03-06 20:21:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function openDrawer() {
+
+    document.getElementById("drawer").classList.add("open")
+    document.getElementById("overlay").classList.add("active")
+
+}
+
+function closeDrawer() {
+
+    document.getElementById("drawer").classList.remove("open")
+    document.getElementById("overlay").classList.remove("active")
+
+}
